aed6652252ba9a6ad184b71a19cf733783b4583c
Andrzej Borucki <borucki.andrzej@gmail.com>
2020-09-22 18:28:16 +0200

    correct align from layout sample

--- src/MessageBox/MessageBox.cpp
+++ src/MessageBox/MessageBox.cpp
@@ -4,24 +4,33 @@
 
 MessageBox::MessageBox(wxWindow *parent, wxWindowID id, const wxString &title)
 : wxDialog(parent, id, title, wxDefaultPosition, wxSize(300,100), wxDEFAULT_FRAME_STYLE) {
-    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
-    wxBoxSizer *sizerUp = new wxBoxSizer(wxHORIZONTAL);
-    wxStaticText *label = new wxStaticText(this, wxID_ANY, "1234567890");
+    wxPanel* p = new wxPanel(this, wxID_ANY);
 
+    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL );
+    wxBoxSizer *top_box = new wxBoxSizer( wxHORIZONTAL );
     const wxSize imageSize(32, 32);
-    wxStaticBitmap *sbmp = new wxStaticBitmap(this, wxID_ANY,
-                                              wxArtProvider::GetIcon(wxART_QUESTION, wxART_OTHER, imageSize),
-                                              wxPoint(10,10), wxSize(32,32));
+    top_box->Add(
+            new wxStaticBitmap(p, wxID_ANY,
+                               wxArtProvider::GetIcon(wxART_QUESTION, wxART_OTHER, imageSize)),
+            wxSizerFlags().Border(wxALL, 7));
+    top_box->Add(
+            new wxStaticText(p, wxID_ANY, "1234567890"),
+            wxSizerFlags().Border(wxALL, 7));
 
-    sizerUp->Add(sbmp, 1,wxCENTER);
-    sizerUp->Add(label, 1, wxRIGHT);
+    wxBoxSizer *button_box = new wxBoxSizer( wxHORIZONTAL );
+    button_box->Add(
+            new wxButton( p, wxID_ANY, "Two buttons in a box" ),
+            wxSizerFlags().Border(wxALL, 7));
+    button_box->Add(
+            new wxButton( p, wxID_ANY, "(wxCENTER)" ),
+            wxSizerFlags().Border(wxALL, 7));
 
-    wxBoxSizer *sizerButtons = new wxBoxSizer(wxHORIZONTAL);
-    wxButton *button = new wxButton(this, 1, "OK");
-    sizerButtons->Add(button, 1,wxCENTER);
-    wxButton *button1 = new wxButton(this, 1, "1");
-    sizerButtons->Add(button1, 1,wxCENTER);
-    sizer->Add(sizerUp);
-    sizer->Add(sizerButtons);
-    SetSizer(sizer);
+    sizer->Add(top_box, wxSizerFlags().Center());
+    sizer->Add(button_box, wxSizerFlags().Center());
+
+    p->SetSizer(sizer );
+
+    // don't allow frame to get smaller than what the sizers tell it and also set
+    // the initial size as calculated by the sizers
+    sizer->SetSizeHints(this );
 }
